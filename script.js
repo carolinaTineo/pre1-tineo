63122848320cb7a4c16936db4784de69b2915159
carolinaTineo <tineo.carolina@hotmail.com>
2022-12-26 21:24:07 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+let ingreseUnNumero=1000 
+
+let numeroIngresado=prompt("ingrese un numero")
+
+if (numeroIngresado > ingreseUnNumero) {
+    alert("el numero ingresado es mayor a 1000")
+} else {
+    alert ("ingrese un valor mayor")
+}
