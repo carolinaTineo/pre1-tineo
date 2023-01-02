7f181baac85a401d6f9e349ac9ec920f25b3e997
carolinaTineo <tineo.carolina@hotmail.com>
2023-01-02 15:21:08 -0300

    cambios

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-let monto= Number (prompt("ingrese monto a invertir"))
-let interesAnual= Number(prompt("ingrese interes anual"))
+let consulta= (prompt("desea calcular un monto si/no"))
+while (consulta=="si") {
+    
+let nombre= (prompt("bienvenido, ¿como es su nombre?"))
+let monto= Number (prompt(nombre + ", ingrese monto a invertir"))
+let interesAnual= Number(prompt(nombre +", ingrese interes anual"))
 let anios= Number(prompt("en cuantos años desea financiar"))
 let total= monto
 
-for (let i = 0; i < anios; i++) {
-    total= total + total * interesAnual / 100
-   console.log(total)
-    
+
+
+function calculador() {
+    for (let i = 0; i < anios; i++) {
+        total= total + total * interesAnual / 100}
+    alert( " su financiacion es " + total)
+        
+}
+calculador()
+alert( " su financiacion es " + total)
+
+
+switch (consulta) {
+    case "si":
+    calculador()
+        
+        break;
+    case "no":
+
+    default:
+        alert("gracias por su visita, lo esperamos pronto")
+        break;
+break
+}
+consulta
 }
